4411b484a8025d201dfcdd872440138c7a1c00e4
NightDreams <yayodcv@gmail.com>
2022-04-01 19:25:07 -0600

    C4 - Setup composition in <App>

--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,52 @@
-import React from 'react';
-import { TodoProvider } from '../TodoContext';
-import { AppUI } from './AppUI';
-
+import React, { useState } from 'react';
+// import { TodoProvider } from '../TodoContext';
+// import { TodoCounter } from '../TodoCounter';
+// import { AppUI } from './AppUI';
 
 function App() {
+  const [state, setState] = useState("estado compartido ");
+
   return (
-    <TodoProvider>
-      <AppUI />
-    </TodoProvider>
-  );
+    <>
+      <TodoHeader>
+        <TodoCounter />
+        <TodoSearch />
+      </TodoHeader>
+
+      <TodoList>
+        <TodoItem state={state} />
+      </TodoList>
+    </>
+  )
 }
+function TodoHeader({ children }) {
+  return (
+    <header> {children}</header>
+  )
+}
+function TodoList({ children }) {
+  return (
+    <section className="Todolist-container">
+      {children}
+    </section>
+  )
+
+}
+function TodoCounter() {
+  return <p>TodoCounter</p>;
+}
+function TodoSearch() {
+  return <p>TodoSearch</p>;
+}
+function TodoItem({ state }) {
+  return <p>TodoItem: {state}</p>;
+}
+// function App() {
+//   return (
+//     <TodoProvider>
+//       <AppUI />
+//     </TodoProvider>
+//   );
+// }
 
 export default App;
