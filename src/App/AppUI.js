e79279dc45c24e3a7cc9cb369f42aceaff115983
NightDreams <yayodcv@gmail.com>
2022-04-03 13:50:53 -0500

    C6 Setup composicion - AppUI  .Remove context  - TodoCounter, TodoSearch and wrap in TodoHeader

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { TodoContext } from '../TodoContext';
+import { TodoHeader } from '../TodoHeader';
 import { TodoCounter } from '../TodoCounter';
 import { TodoSearch } from '../TodoSearch';
 import { TodoList } from '../TodoList';
@@ -20,18 +21,25 @@ function AppUI() {
     deleteTodo,
     openModal,
     setOpenModal,
+    //props for TodoCounter
+    totalTodos,
+    completedTodos,
+    //props for TodoSearch
+    searchValue,
+    setSearchValue,
   } = React.useContext(TodoContext);
-  
+
   return (
     <React.Fragment>
-      <TodoCounter />
-      <TodoSearch />
-
+      <TodoHeader>
+        <TodoCounter totalTodos={totalTodos} completedTodos={completedTodos} />
+        <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
+      </TodoHeader>
       <TodoList>
         {error && <TodosError />}
         {loading && <TodosLoading />}
         {(!loading && !searchedTodos.length) && <EmptyTodos />}
-        
+
         {searchedTodos.map(todo => (
           <TodoItem
             key={todo.text}
--- src/App/index.js
+++ src/App/index.js
@@ -1,52 +1,13 @@
-import React, { useState } from 'react';
-// import { TodoProvider } from '../TodoContext';
-// import { TodoCounter } from '../TodoCounter';
-// import { AppUI } from './AppUI';
+import React from 'react';
+import { TodoProvider } from '../TodoContext';
+import { AppUI } from './AppUI';
 
 function App() {
-  const [state, setState] = useState("estado compartido ");
-
   return (
-    <>
-      <TodoHeader>
-        <TodoCounter />
-        <TodoSearch />
-      </TodoHeader>
-
-      <TodoList>
-        <TodoItem state={state} />
-      </TodoList>
-    </>
-  )
+    <TodoProvider>
+      <AppUI />
+    </TodoProvider>
+  );
 }
-function TodoHeader({ children }) {
-  return (
-    <header> {children}</header>
-  )
-}
-function TodoList({ children }) {
-  return (
-    <section className="Todolist-container">
-      {children}
-    </section>
-  )
-
-}
-function TodoCounter() {
-  return <p>TodoCounter</p>;
-}
-function TodoSearch() {
-  return <p>TodoSearch</p>;
-}
-function TodoItem({ state }) {
-  return <p>TodoItem: {state}</p>;
-}
-// function App() {
-//   return (
-//     <TodoProvider>
-//       <AppUI />
-//     </TodoProvider>
-//   );
-// }
 
 export default App;
