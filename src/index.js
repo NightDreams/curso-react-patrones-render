6c6cf995f683f88bd51fb72982d8b19012e09711
NightDreams <yayodcv@gmail.com>
2022-04-08 20:16:02 -0500

    C13 - Setup first HOC

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App/index.js';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+// import App from './App/index.js';
+import "./index.css";
+
+function App(props) {
+  return (
+    <h1>
+      {props.saludo}, {props.name}!
+    </h1>
+  );
+}
+// 1- setup HOC
+function withSaludo(saludo) {
+  return function WrappedComponentWithSaludo(WrappedComponent) {
+    return function ComponenteDeVerdad(props) {
+      return (
+        <>
+          <WrappedComponent {...props} saludo={saludo} />
+          <p>Estamos acompañando al WrappedComponent</p>
+        </>
+      );
+    };
+  };
+}
+
+// 2 instance HOC and  - Donde vamos a conectar el HOC  - feature
+const AppWithSaludo = withSaludo("wenas")(App);
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <AppWithSaludo name={"juanita"} />,
+  document.getElementById("root")
 );
